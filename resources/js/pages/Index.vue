df19125c16ff9c641190a457677b83954d4c9725
Kalok Lau <kalok@parcelpro.nl>
2025-06-02 13:41:23 +0200

    Link op homepage naar /shipments/create

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,21 +1,7 @@
-<script setup lang="ts">
-import axios from 'axios';
-
-const props = defineProps({
-    order: Object
-});
-
-const synchronize = async () => {
-    await axios.post('/shipments/store', {
-        order: props.order
-    }).then(() => {
-        window.location.href = '/shipments';
-    });
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <div class="grid h-screen place-items-center">
-        <button class="p-2 rounded text-white bg-blue-500" @click.prevent="synchronize">Order(s) ophalen</button>
+        <a class="p-2 rounded text-white bg-blue-500" href="/shipments/create">Zending aanmaken</a>
     </div>
 </template>
